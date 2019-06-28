d8c9549375be368f181a924235c32fb6417cb481
trepixert <pixert@mail.ru>
2019-06-28 04:45:43 +0400

    final variant v2.0 :D

--- src/main/java/task/first/Implementations/HandlerImpl.java
+++ src/main/java/task/first/Implementations/HandlerImpl.java
@@ -18,6 +18,25 @@ public class HandlerImpl implements Handler {
         parserAndCalc = new ExpressionParserAndCalcImpl();
     }
 
+    /**
+     * Метод обработки, введенного пользователем, выражения
+     * Сначала идет проверка на наличие необходимой валюты, далее обработка и привод выражения
+     * к той валюте, которую изначально ввел пользователь, например:
+     * если валюта доллар, то методом обработки выражения будет:
+     * @see HandlerImpl#handlerToDollars(String)
+     * если валюта рубль, то:
+     * @see HandlerImpl#handlerToRubles(String)
+     * Смысл обработки в том, чтобы конвертировать все выражение к одной валюте
+     * Далее выражение обрабатывается обратной польской нотацией:
+     * @see ExpressionParserAndCalcImpl#parse(String)
+     * и вычисляется:
+     * @see ExpressionParserAndCalcImpl#calc(List)
+     *
+     * @param currency валюта
+     * @param expression выражение, которое необходимо посчитать
+     * @return результатом является выполнение выражения
+     * @throws Exception в случае, если введенная пользователем валюта не обнаружится
+     */
     @Override
     public double doHandlerAndGetResult(String currency, String expression) throws Exception {
         if (!currencies.contains(currency)) {
@@ -50,6 +69,11 @@ public class HandlerImpl implements Handler {
         return result;
     }
 
+    /**
+     *
+     * @param expression выражение, которое необходимо конвертировать
+     * @return конвертированное выражение в рублях
+     */
     private String handlerToRubles(String expression) {
         editExpression = new StringBuilder(expression);
         String[] splitByDollars = handleExpressionToConvert(true, '$').split("/");
@@ -65,6 +89,11 @@ public class HandlerImpl implements Handler {
         return new String(editExpression);
     }
 
+    /**
+     *
+     * @param expression выражение, которое необходимо конвертировать
+     * @return конвертированное выражение в долларах
+     */
     private String handlerToDollars(String expression) {
         editExpression = new StringBuilder(expression);
         String[] splitByRoubles = handleExpressionToConvert(false, 'p').split("/");
@@ -80,6 +109,24 @@ public class HandlerImpl implements Handler {
         return new String(editExpression);
     }
 
+    /**
+     * Чтобы было удобнее выделить, засплитить и конвертировать нужные числа, я их "завернул" в /,
+     * в первом случае применяется реверсированное выделение числа, то есть сначала ищется число с интересующей
+     * валютой, например рубль: 737р р находится справа, поэтому чтобы выделить данное число (поместить / перед 7
+     * Итоговый вариант: /737p/) нужно уйти на несколько индексов влево, что и делается в теле if
+     * во втором случае применяется линейной выделение числа, например: ($85.4 -> итоговый вариант /$85.4/)
+     * перед валютой ставится / и дальше идёт обычный переход по индексам, пока не будет достигнут один из символов (+, -,(
+     * или дойдёт до конца строки) и далее при возврате строки к функции, что её вызвала начинается сплит по /, таким образом
+     * мы отделяем нужные нам числа от других
+     *
+     * @param isReversed поскольку валюта в рублях пишется справа, а в долларах слева, то
+     *                   я принял решение разделить поиск чисел на два вида:
+     *                   в первом случае, если валюта, аналогично рублю, будет находиться справа
+     *                   от числа, то применяется реверсированное выделение числа, а иначе
+     *                   линейное (объяснение выше)
+     * @param currency валюта, которую нужно конвертировать
+     * @return строку с установленными слэшами, для последующего сплита и конвертации
+     */
     private String handleExpressionToConvert(boolean isReversed, char currency) {
         char currencyAtExpression;
         if (isReversed) {
